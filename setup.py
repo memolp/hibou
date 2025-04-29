61b2bd6c8bd2fd5d437b8d2142b7e8ac3ededc37
memolp <memolp@163.com>
2025-04-29 18:32:48 +0800

    提交打包结构

--- setup.py
+++ setup.py
@@ -6,12 +6,26 @@
 2. 将pyd文件和其他依赖的py文件打包成whl包
 """
 
-from setuptools import setup, extension
+import sys
+
+from setuptools import setup, find_packages
 from Cython.Build import cythonize
 
-setup(
-    name="hibou",
-    version="1.0.0",
-    author="Jeff Xun",
-    ext_modules= cythonize("hibou.py"))
+if "build_ext" in sys.argv:
+    setup(
+        name="hibou",
+        version="1.0.0",
+        author="Jeff Xun",
+        ext_modules= cythonize("hibou.py"))
 
+else:
+    setup(
+        name='hibou',
+        version='0.1',
+        packages=find_packages(),
+        install_requires=[],  # 如果有依赖，添加在这里
+        package_data={
+            'hibou': ['*.pyd', '*.pyi'],  # 确保 .pyd 和 .pyi 文件被包含
+        },
+        include_package_data=True,
+    )
